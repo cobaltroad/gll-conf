325d4e17064eb7066c2a73d664597b2c3e176b2f
Ron Dollete <ron@cobaltroad.com>
2017-11-24 13:54:25 -0500

    Now passing

--- app/interactors/authenticating/find_user.rb
+++ app/interactors/authenticating/find_user.rb
@@ -2,5 +2,9 @@ class Authenticating::FindUser
   include Interactor
 
   def call
+    context.user = User.find_by_email(context.email)
+    unless context.user
+      context.fail!(error: "Login error", message: "Cannot find email")
+    end
   end
 end
