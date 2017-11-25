969e01bf02b93c6da56820099f3a258a5d1b6381
Ron Dollete <ron@cobaltroad.com>
2017-11-25 12:37:49 -0500

    Switched interactor context to match model attribute

--- app/interactors/questioning/add_question.rb
+++ app/interactors/questioning/add_question.rb
@@ -2,10 +2,10 @@ class Questioning::AddQuestion
   include Interactor
 
   before do
-    context.fail!(message: "Question body cannot be blank") if context.question.blank?
+    context.fail!(message: "Question body cannot be blank") if context.body.blank?
   end
 
   def call
-    Question.create(user: context.user, body: context.question)
+    context.question = Question.create(user: context.user, body: context.body)
   end
 end
--- test/interactors/questioning/add_question_test.rb
+++ test/interactors/questioning/add_question_test.rb
@@ -7,21 +7,21 @@ class AddQuestionInteractorTest < ActiveSupport::TestCase
 
   test "user adds a question" do
     assert_difference ->{ Question.count } do
-      result = Questioning::AddQuestion.call(user: @user, question: "New Question?")
+      result = Questioning::AddQuestion.call(user: @user, body: "New Question?")
       assert result.success?
     end
   end
 
   test "question is a blank string" do
     assert_no_difference ->{ Question.count } do
-      result = Questioning::AddQuestion.call(user: @user, question: "")
+      result = Questioning::AddQuestion.call(user: @user, body: "")
       assert_not result.success?
     end
   end
 
   test "question is nil" do
     assert_no_difference ->{ Question.count } do
-      result = Questioning::AddQuestion.call(user: @user, question: nil)
+      result = Questioning::AddQuestion.call(user: @user, body: nil)
       assert_not result.success?
     end
   end
