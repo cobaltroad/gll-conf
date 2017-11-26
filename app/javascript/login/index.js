ac5870054d35825ab5c7dd92f59ca33db65a294e
Ron Dollete <ron@cobaltroad.com>
2017-11-26 14:21:53 -0500

    Barebones login form

---
+++ app/javascript/login/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import LoginComponent from './login-component'
+
+const login = document.querySelector('#login')
+ReactDOM.render(<LoginComponent />, login)
