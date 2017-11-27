55aa15e8804bc08a40c50dce0dbea8ec6741f6ce
Ron Dollete <ron@cobaltroad.com>
2017-11-26 22:38:34 -0500

    Removing login page from Rails router

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resource :authentication, only: [:show, :create]
+  resource :authentication, only: [:create]
   resources :questions, only: [:index, :create, :update] do
     member do
       post :vote
