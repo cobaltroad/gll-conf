379ade3ee74ce0b3c726f2a077516566a99a7919
Ron Dollete <ron@cobaltroad.com>
2017-11-28 19:18:24 -0500

    Read and set state with each checkbox click

--- app/javascript/conference/questions/select-question-component.jsx
+++ app/javascript/conference/questions/select-question-component.jsx
@@ -1,18 +1,29 @@
 import React from 'react'
+import HttpClient from '../http-client/http-client'
 
 export default class SelectQuestion extends React.Component {
   constructor(props) {
     super(props);
     this.state = { ...props }
+
+    this.httpClient = HttpClient.instance;
   }
 
-  selectQuestion = (e) => {
-    console.log("SELECTING", e.target.value);
+  toggleSelection = (e) => {
+    const newValue = !this.state.is_selected;
+    const updateObj = {
+      id: this.state.id,
+      is_selected: newValue
+    };
+
+    this.httpClient.updateQuestion(updateObj).then((question) => {
+      this.setState({ is_selected: newValue });
+    });
   }
 
   render() {
     return(
-      <input type="checkbox" checked={this.state.is_selected} onClick={this.selectQuestion} />
+      <input type="checkbox" checked={this.state.is_selected} onChange={this.toggleSelection} />
     );
   }
 }
